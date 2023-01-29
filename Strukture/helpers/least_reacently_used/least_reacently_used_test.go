f899e0a25caaf7ba77c3784165cd95b0389ddeac
OgnjenGligoric <gligoric383@gmail.com>
2023-01-29 17:03:38 +0100

    zavrsen lru

--- Strukture/helpers/least_reacently_used/least_reacently_used_test.go
+++ Strukture/helpers/least_reacently_used/least_reacently_used_test.go
@@ -9,17 +9,23 @@ func TestCache(t *testing.T) {
 	if u != "beograd" {
 		t.Fatalf("Ocekivano 'beograd', dobijeno '%s'", u)
 	}
+
+	cache.Set("uk", "manchester")
+	u = cache.Get("uk")
+	if u != "manchester" {
+		t.Fatalf("Expected 'manchester', got '%s'", u)
+	}
 }
 
 func TestLRU(t *testing.T) {
 	cache := NewLRU(3)
 	cache.Set("uk", "london")
 	cache.Set("france", "beograd")
-	cache.set("germany", "mongol")
+	cache.Set("germany", "mongol")
 
 	cache.Set("belgium", "brussels")
 
-	u := cache.get("uk")
+	u := cache.Get("uk")
 	if u != "" {
 		t.Fatalf("uk should no longer be preasent")
 	}
@@ -28,7 +34,7 @@ func TestLRU(t *testing.T) {
 
 	f := cache.Get("france")
 
-	if f != "paris" {
+	if f != "beograd" {
 		t.Fatalf("expected 'paris', but got %s", f)
 	}
 	// red : france - belgium - germany
