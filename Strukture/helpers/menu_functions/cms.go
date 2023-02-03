ddaaafc200326b4a51396b24116a9e1a6c193235
maric300 <branko.maric300@gmail.com>
2023-02-03 17:08:22 +0100

    cms update, hll deserialisation

--- Strukture/helpers/menu_functions/cms.go
+++ Strukture/helpers/menu_functions/cms.go
@@ -14,7 +14,7 @@ import (
 func CreateCountMinSketch(mem MemTable, lru *LRUCache, bucket *TokenBucket) (string, *CountMinSketch) {
 	var input string
 	cms := new(CountMinSketch)
-	for true {
+	for true{
 
 		fmt.Print("Unesite kljuc: ")
 		fmt.Scanln(&input)
@@ -22,7 +22,7 @@ func CreateCountMinSketch(mem MemTable, lru *LRUCache, bucket *TokenBucket) (str
 		found, _ := GET(input, mem, lru, bucket)
 		if found == true {
 			fmt.Println("Takav kljuc vec postoji u bazi podataka. Molimo vas unesite drugi.")
-		} else {
+		}else {
 			var epsilon float64
 			var delta float64
 
@@ -37,10 +37,9 @@ func CreateCountMinSketch(mem MemTable, lru *LRUCache, bucket *TokenBucket) (str
 		}
 	}
 
-	return input, cms
+	return input, cms //TODO Isrpavi
 }
-
-// dobavlja cms iz baze podataka
+//dobavlja cms iz baze podataka
 func CountMinSketchGET(mem MemTable, lru *LRUCache, bucket *TokenBucket) (bool, string, *CountMinSketch) {
 	var key string
 	cms := new(CountMinSketch)
@@ -49,7 +48,7 @@ func CountMinSketchGET(mem MemTable, lru *LRUCache, bucket *TokenBucket) (bool, 
 	fmt.Print("Unesite kljuc: ")
 	fmt.Scanln(&key)
 	key = "CountMinSketch" + key
-
+	
 	found, data := GET(key, mem, lru, bucket)
 	if found {
 		cmsBytes := data.Value
@@ -91,10 +90,19 @@ func CountMinSketchPUT(key string, cms *CountMinSketch, mem MemTable, bucket *To
 	PUT(key, data, mem, bucket)
 }
 
+
+
 func CountMinSKetchMenu(mem MemTable, lru *LRUCache, bucket *TokenBucket) {
 	activeCMS := new(CountMinSketch)
-	var activeKey string
+	var activeKey string //kljuc CMS-a
+	var userkey string //kljuc koji je korisnik uneo i koji se ispisuje korisniku
+	userkey = ""
 	for true {
+
+		fmt.Println("=======================================")
+		fmt.Print("Kljuc aktivnog CountMinSketch-a: ")
+		fmt.Println(userkey)
+		fmt.Println()
 		fmt.Println("1 - Kreiraj CountMinSketch")
 		fmt.Println("2 - Dobavi CountMinSketch iz baze podataka")
 		fmt.Println("3 - Dodaj element")
@@ -118,22 +126,42 @@ func CountMinSKetchMenu(mem MemTable, lru *LRUCache, bucket *TokenBucket) {
 		switch input {
 		case "1":
 			activeKey, activeCMS = CreateCountMinSketch(mem, lru, bucket)
+			userkey = activeKey[14:]
 		case "2":
 			found, key, tempCMS := CountMinSketchGET(mem, lru, bucket)
 			if found {
 				activeCMS = tempCMS
 				activeKey = key
+				userkey = activeKey[14:]
 			} else {
 				fmt.Println("Ne postoji CountMinSKetch sa datim kljucem")
 			}
 		case "3":
-			CountMinSketchAddElement(activeCMS)
+
+			if len(activeKey) != 0 {
+				CountMinSketchAddElement(activeCMS)
+			} else{
+				fmt.Println("Nije izabran aktivni CMS")
+			}
+
 		case "4":
-			CountMinSketchCheckFrequency(activeCMS)
+			if len(activeKey) != 0 {
+				CountMinSketchCheckFrequency(activeCMS)
+			} else{
+				fmt.Println("Nije izabran aktivni CMS")
+			}
 		case "5":
-			CountMinSketchPUT(activeKey, activeCMS, mem, bucket, false)
+			if len(activeKey) != 0 {
+				CountMinSketchPUT(activeKey, activeCMS, mem, bucket, false)
+			} else{
+				fmt.Println("Nije izabran aktivni CMS")
+			}
 		case "6":
-			CountMinSketchPUT(activeKey, activeCMS, mem, bucket, true)
+			if len(activeKey) != 0 {
+				CountMinSketchPUT(activeKey, activeCMS, mem, bucket, true)
+			} else{
+				fmt.Println("Nije izabran aktivni CMS")
+			}
 		case "x":
 			fmt.Println("Vidimo se sledeci put!")
 			os.Exit(0)
@@ -144,5 +172,4 @@ func CountMinSKetchMenu(mem MemTable, lru *LRUCache, bucket *TokenBucket) {
 			fmt.Println("Neispravan unos. Molimo vas probajte opet.")
 		}
 	}
-
 }
