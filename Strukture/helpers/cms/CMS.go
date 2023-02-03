09ffd8a6d21e73b6c7b5be5e0ba500ebf87f099a
maric300 <branko.maric300@gmail.com>
2023-02-03 04:08:41 +0100

    CMS Menu (not tested)

--- Strukture/helpers/cms/CMS.go
+++ Strukture/helpers/cms/CMS.go
@@ -1,6 +1,9 @@
 package cms
 
 import (
+	"bytes"
+	"encoding/binary"
+	"log"
 	"math"
 )
 
@@ -35,7 +38,7 @@ func AddToCms(cms *CountMinSketch, elem []byte) {
 }
 
 func CheckFrequencyInCms(cms *CountMinSketch, elem []byte) uint32 {
-	//Pomocni slice pomocu kojeg racunam min (sastoji se od svih vrednosti)
+	//Pomocni slice pomocu kojeg racuna min (sastoji se od svih vrednosti)
 	arr := make([]uint32, cms.K)
 	for i, fn := range cms.HashFuncs {
 		hashedValue := int(math.Mod(float64(fn.Hash(elem)), float64(cms.M)))
@@ -48,4 +51,125 @@ func CheckFrequencyInCms(cms *CountMinSketch, elem []byte) uint32 {
 		}
 	}
 	return min
+}
+
+func CountMinSkechToBytes(cms *CountMinSketch) []byte {
+
+	//upisujemo promenljive tipa uint32
+	bytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(bytes, uint32(cms.K))
+
+	bytesM := make([]byte, 4)
+	binary.BigEndian.PutUint32(bytesM, uint32(cms.M))
+	bytes = append(bytes, bytesM...)
+
+	//upisujemo promenljive tipa float64
+	bitsEpsilon := math.Float64bits(cms.Epsilon)
+	bytesEpsilon := make([]byte, 8)
+	binary.BigEndian.PutUint64(bytesEpsilon, bitsEpsilon)
+	bytes = append(bytes, bytesEpsilon...)
+
+	bitsDelta := math.Float64bits(cms.Epsilon)
+	bytesDelta := make([]byte, 8)
+	binary.BigEndian.PutUint64(bytesDelta, bitsDelta)
+	bytes = append(bytes, bytesDelta...)
+
+	//upisujemo podatak po podatak iz valuetable-a
+	for i:= 0; i < int(cms.K); i++ {
+		for j:= 0; j < int(cms.M); j++ {
+			bytesElem := make([]byte, 4)
+			binary.BigEndian.PutUint32(bytesElem, uint32(cms.ValueTable[i][j]))
+			bytes = append(bytes, bytesElem...)
+		}
+	}
+
+	for _, fn := range cms.HashFuncs {
+		//Belezimo duzinu svake hashfunkcije
+		bytesHFLen := make([]byte, 4)
+		binary.BigEndian.PutUint32(bytesHFLen, uint32(len(fn.Seed)))
+		bytes = append(bytes, bytesHFLen...)
+
+		//zapisuje hashfunkciju
+		bytes = append(bytes, fn.Seed...)
+	}
+	return bytes
+}
+
+func BytesToCountMinSketch(CmsBytes []byte) *CountMinSketch {
+	cms := new(CountMinSketch)
+	reader := bytes.NewReader(CmsBytes)
+	bytes := make([]byte, 4)
+
+	//Ucitavamo podatke tipa uint32
+	_, err := reader.Read(bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cms.K = binary.BigEndian.Uint32(bytes)
+
+	bytes = make([]byte, 4)
+	_, err = reader.Read(bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cms.M = binary.BigEndian.Uint32(bytes)
+
+	//ucitavamo podatke tipa float64
+	bytes = make([]byte, 8)
+	_, err = reader.Read(bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bitsEpsilon := binary.BigEndian.Uint64(bytes)
+	floatEpsilon := math.Float64frombits(bitsEpsilon)
+	cms.Epsilon = floatEpsilon
+
+	bytes = make([]byte, 8)
+	_, err = reader.Read(bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bitsDelta := binary.BigEndian.Uint64(bytes)
+	floatDelta := math.Float64frombits(bitsDelta)
+	cms.Delta = floatDelta
+
+	//kreiramo ValueTable
+	cms.ValueTable = make([][]uint32, cms.K)
+	for i := range cms.ValueTable {
+		cms.ValueTable[i] = make([]uint32, cms.M)
+	}
+
+	for i:= 0; i < int(cms.K); i++ {
+		for j:= 0; j < int(cms.M); j++ {
+			bytes := make([]byte, 4)
+			_, err = reader.Read(bytes)
+			if err != nil {
+				log.Fatal(err)
+			}
+			cms.ValueTable[i][j] = binary.BigEndian.Uint32(bytes)
+		}
+	}
+
+	cms.HashFuncs = make([]CmsHashWithSeed, 0)
+	hashWithSeed := new(CmsHashWithSeed)
+	//Ucitavamo svaku hashfunkciju
+	for i := uint32(0); i < cms.K; i++ {
+		//Ucitavamo duzinu trenutne hf
+		bytes = make([]byte, 4)
+		_, err = reader.Read(bytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+		hashFuncLen := binary.BigEndian.Uint32(bytes)
+
+		//citamo hf
+		bytes = make([]byte, hashFuncLen)
+		_, err = reader.Read(bytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+		hashWithSeed.Seed = bytes
+		cms.HashFuncs = append(cms.HashFuncs, *hashWithSeed)
+	}
+	return cms
 }
--- Strukture/helpers/menu_functions/menu_functions.go
+++ Strukture/helpers/menu_functions/menu_functions.go
@@ -2,6 +2,8 @@ package menu_functions
 
 import (
 	"fmt"
+	"os"
+	. "project/gosaomi/cms"
 	. "project/gosaomi/dataType"
 	. "project/gosaomi/entry"
 	. "project/gosaomi/least_reacently_used"
@@ -163,4 +165,141 @@ func SimHashCompare(key1 string, key2 string, mem MemTable, lru *LRUCache, bucke
 
 	return Compare(data1.Value, data2.Value)
 
+}
+
+func CreateCountMinSketch(mem MemTable, lru *LRUCache, bucket *TokenBucket) (string, *CountMinSketch) {
+	var input string
+	cms := new(CountMinSketch)
+	for true{
+
+		fmt.Print("Unesite kljuc: ")
+		fmt.Scanln(&input)
+		input = "CountMinSketch" + input
+		found, _ := GET(input, mem, lru, bucket)
+		if found == true {
+			fmt.Println("Takav kljuc vec postoji u bazi podataka. Molimo vas unesite drugi.")
+		}else {
+			var epsilon float64
+			var delta float64
+
+			//TODO: dodaj validacije
+			fmt.Print("Unesite preciznost (epsilon): ")
+			fmt.Scanln(&epsilon)
+			fmt.Print("Unesite sigurnost tacnosti (delta): ")
+			fmt.Scanln(&delta)
+			cms = NewCountMinSketch(epsilon, delta)
+
+			break
+		}
+	}
+
+	return input, cms
+}
+//dobavlja cms iz baze podataka
+func CountMinSketchGET(mem MemTable, lru *LRUCache, bucket *TokenBucket) (bool, string, *CountMinSketch) {
+	var key string
+	cms := new(CountMinSketch)
+
+	//unos
+	fmt.Print("Unesite kljuc: ")
+	fmt.Scanln(&key)
+	key = "CountMinSketch" + key
+	
+	found, data := GET(key, mem, lru, bucket)
+	if found {
+		cmsBytes := data.Value
+		cms = BytesToCountMinSketch(cmsBytes)
+		return true, key, cms
+	}
+	return false, key, cms
+
+}
+
+func CountMinSketchAddElement(cms *CountMinSketch) {
+	var val []byte
+
+	//unos
+	fmt.Print("Unesite podatak koji zelite da dodate: ")
+	fmt.Scanln(&val)
+	AddToCms(cms, val)
+}
+
+func CountMinSketchCheckFrequency(cms *CountMinSketch) {
+	var val []byte
+
+	//unos
+	fmt.Print("Unesite podatak koji zelite da dodate: ")
+	fmt.Scanln(&val)
+
+	freq := CheckFrequencyInCms(cms, val)
+
+	fmt.Print("Broj ponavljanja podatka iznosi: ")
+	fmt.Println(freq)
+}
+
+func CountMinSketchPUT(key string, cms *CountMinSketch, mem MemTable, bucket *TokenBucket, tombstone bool) {
+	data := new(Data)
+	bytesCms := CountMinSkechToBytes(cms)
+	data.Value = bytesCms
+	data.Timestamp = uint64(time.Now().Unix())
+	data.Tombstone = tombstone
+	PUT(key, data, mem, bucket)
+}
+
+
+
+func CountMinSKetchMenu(mem MemTable, lru *LRUCache, bucket *TokenBucket) {
+	activeCMS := new(CountMinSketch)
+	var activeKey string
+	for true {
+		fmt.Println("1 - Kreiraj CountMinSketch")
+		fmt.Println("2 - Dobavi CountMinSketch iz baze podataka")
+		fmt.Println("3 - Dodaj element")
+		fmt.Println("4 - Proveri broj ponavljanja")
+		fmt.Println("5 - Upisi CountMinSketch u bazu podataka")
+		fmt.Println("6 - Obrisi CountMinSketch iz baze podataka")
+		fmt.Println("X - Izlaz iz programa")
+		fmt.Println("=======================================")
+		fmt.Print("Izaberite opciju: ")
+
+		var input string
+		n, err := fmt.Scanln(&input)
+
+		if err != nil {
+			fmt.Println("Greska prilikom unosa: ", err)
+		} else if n == 0 {
+			fmt.Println("Prazan unos.  Molimo vas probajte opet.")
+			return
+		}
+
+		switch input {
+		case "1":
+			activeKey, activeCMS = CreateCountMinSketch(mem, lru, bucket)
+		case "2":
+			found, key, tempCMS := CountMinSketchGET(mem, lru, bucket)
+			if found {
+				activeCMS = tempCMS // da li ovo ovako radi ili ipak mora deep copy?
+				activeKey = key
+			} else {
+				fmt.Println("Ne postoji CountMinSKetch sa datim kljucem")
+			}
+		case "3":
+			CountMinSketchAddElement(activeCMS)
+		case "4":
+			CountMinSketchCheckFrequency(activeCMS)
+		case "5":
+			CountMinSketchPUT(activeKey, activeCMS, mem, bucket, false)
+		case "6":
+			CountMinSketchPUT(activeKey, activeCMS, mem, bucket, true)
+		case "x":
+			fmt.Println("Vidimo se sledeci put!")
+			os.Exit(0)
+		case "X":
+			fmt.Println("Vidimo se sledeci put!")
+			os.Exit(0)
+		default:
+			fmt.Println("Neispravan unos. Molimo vas probajte opet.")
+		}
+	}
+	
 }
