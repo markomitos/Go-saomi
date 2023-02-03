fdbd23b14c7b7bc92fb86b42e740f7e61adc1526
maric300 <branko.maric300@gmail.com>
2023-02-03 15:40:00 +0100

    CMS update

--- Strukture/helpers/SSTable/sstable.go
+++ Strukture/helpers/SSTable/sstable.go
@@ -5,10 +5,8 @@ import (
 	"hash/crc32"
 	"io"
 	"log"
-	"math"
 	"os"
 	. "project/gosaomi/bloom"
-	. "project/gosaomi/cms"
 	. "project/gosaomi/config"
 	. "project/gosaomi/dataType"
 	. "project/gosaomi/entry"
@@ -364,125 +362,7 @@ func byteToBloomFilter(file *os.File) *BloomFilter {
 }
 
 
-func CountMinSkechToBytes(cms *CountMinSketch) []byte {
 
-	//upisujemo promenljive tipa uint32
-	bytes := make([]byte, 4)
-	binary.BigEndian.PutUint32(bytes, uint32(cms.K))
-
-	bytesM := make([]byte, 4)
-	binary.BigEndian.PutUint32(bytesM, uint32(cms.M))
-	bytes = append(bytes, bytesM...)
-
-	//upisujemo promenljive tipa float64
-	bitsEpsilon := math.Float64bits(cms.Epsilon)
-	bytesEpsilon := make([]byte, 8)
-	binary.BigEndian.PutUint64(bytesEpsilon, bitsEpsilon)
-	bytes = append(bytes, bytesEpsilon...)
-
-	bitsDelta := math.Float64bits(cms.Epsilon)
-	bytesDelta := make([]byte, 8)
-	binary.BigEndian.PutUint64(bytesDelta, bitsDelta)
-	bytes = append(bytes, bytesDelta...)
-
-	//upisujemo podatak po podatak iz valuetable-a
-	for i:= 0; i < int(cms.K); i++ {
-		for j:= 0; j < int(cms.M); j++ {
-			bytesElem := make([]byte, 4)
-			binary.BigEndian.PutUint32(bytesElem, uint32(cms.ValueTable[i][j]))
-			bytes = append(bytes, bytesElem...)
-		}
-	}
-
-	for _, fn := range cms.HashFuncs {
-		//Belezimo duzinu svake hashfunkcije
-		bytesHFLen := make([]byte, 4)
-		binary.BigEndian.PutUint32(bytesHFLen, uint32(len(fn.Seed)))
-		bytes = append(bytes, bytesHFLen...)
-
-		//zapisuje hashfunkciju
-		bytes = append(bytes, fn.Seed...)
-	}
-	return bytes
-}
-
-func BytesToCountMinSketch(file *os.File) *CountMinSketch {
-	cms := new(CountMinSketch)
-	bytes := make([]byte, 4)
-
-	//Ucitavamo podatke tipa uint32
-	_, err := file.Read(bytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-	cms.K = binary.BigEndian.Uint32(bytes)
-
-	bytes = make([]byte, 4)
-	_, err = file.Read(bytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-	cms.M = binary.BigEndian.Uint32(bytes)
-
-	//ucitavamo podatke tipa float64
-	bytes = make([]byte, 8)
-	_, err = file.Read(bytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bitsEpsilon := binary.BigEndian.Uint64(bytes)
-	floatEpsilon := math.Float64frombits(bitsEpsilon)
-	cms.Epsilon = floatEpsilon
-
-	bytes = make([]byte, 8)
-	_, err = file.Read(bytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bitsDelta := binary.BigEndian.Uint64(bytes)
-	floatDelta := math.Float64frombits(bitsDelta)
-	cms.Delta = floatDelta
-
-	//kreiramo ValueTable
-	cms.ValueTable = make([][]uint32, cms.K)
-	for i := range cms.ValueTable {
-		cms.ValueTable[i] = make([]uint32, cms.M)
-	}
-
-	for i:= 0; i < int(cms.K); i++ {
-		for j:= 0; j < int(cms.M); j++ {
-			bytes := make([]byte, 4)
-			_, err = file.Read(bytes)
-			if err != nil {
-				log.Fatal(err)
-			}
-			cms.ValueTable[i][j] = binary.BigEndian.Uint32(bytes)
-		}
-	}
-
-	cms.HashFuncs = make([]CmsHashWithSeed, 0)
-	hashWithSeed := new(CmsHashWithSeed)
-	//Ucitavamo svaku hashfunkciju
-	for i := uint32(0); i < cms.K; i++ {
-		//Ucitavamo duzinu trenutne hf
-		bytes = make([]byte, 4)
-		_, err = file.Read(bytes)
-		if err != nil {
-			log.Fatal(err)
-		}
-		hashFuncLen := binary.BigEndian.Uint32(bytes)
-
-		//citamo hf
-		bytes = make([]byte, hashFuncLen)
-		_, err = file.Read(bytes)
-		if err != nil {
-			log.Fatal(err)
-		}
-		hashWithSeed.Seed = bytes
-		cms.HashFuncs = append(cms.HashFuncs, *hashWithSeed)
-	}
-	return cms
-}
 
 func HyperLogLogToBytes(hll *HLL) []byte {
 
